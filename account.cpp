f5a2cb8c5a73988616385fc4b7b9584e4488e2f1
mah9994 <143502031+mah9994@users.noreply.github.com>
2023-09-12 11:27:14 +0000

    done

--- account.cpp
+++ account.cpp
@@ -12,8 +12,11 @@
 //    d. Function 'withdraw' that takes an amount and subtracts it from the balance.
 // 4. A destructor that displays a message like "Account with balance <balance_value> is being destroyed."
 
-#ifndef ACCOUNT_H
-#define ACCOUNT_H
+//#ifndef ACCOUNT_H
+//#define ACCOUNT_H
+#include <iostream>
+//#include <account.h>
+using namespace std;
 
 class Account {
 private:
@@ -24,26 +27,52 @@ public:
     // Default constructor.
     // Initialize 'balance' to 0.
     // TODO: Implement the default constructor.
+    Account() {
+        balance = 0;
+    }
 
     // Parameterized constructor.
     // Initialize 'balance' with the provided 'initialBalance'.
     // TODO: Implement the parameterized constructor.
+    Account(double initialBalance) {
+        balance = initialBalance;
+    }
 
     // Getter function to return the current balance.
     // TODO: Implement the 'getBalance' function.
+    double getBalance() {
+        return balance;
+    }
 
     // Setter function to set the balance to a new value.
     // TODO: Implement the 'setBalance' function.
+    void setBalance(double newBalance) {
+        balance = newBalance;
+    }
 
     // Function to deposit an amount into the account.
     // TODO: Implement the 'deposit' function.
+    void deposit(double amount) {
+        balance += amount;
+    }
 
     // Function to withdraw an amount from the account.
     // TODO: Implement the 'withdraw' function.
+    void withdraw(double amount) {
+        if(amount > balance) {
+            cout << "Insufficient funds!\n";
+        } else {
+            balance -= amount;
+        }
+        
+    }
 
     // Destructor.
     // Display a message indicating the account is being destroyed.
     // TODO: Implement the destructor.
+    ~Account() {
+        cout << "Account with balance " << balance << " is being destroyed.\n";
+    }
 };
 
-#endif // ACCOUNT_H
+//#endif // ACCOUNT_H
